77657eb479464a78b844f03596fafaf2db266aae
Bhuvanesh Waran <bhuvaneshk0001@gmail.com>
2026-02-17 17:43:30 +0530

    A update in std

--- WebApplication1/obj/Debug/net10.0/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/obj/Debug/net10.0/WebApplication1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4d8c70a6c7145338ebf9ef09ed935bd4477f46c1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8e5d03275e01cfc2808ae9e5ebafe5ab52d0890a")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
